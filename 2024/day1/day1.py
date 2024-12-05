2407cf5d49d3af6c1b3f8106ec2561e8ce06763a
uikromm <u.ikromm@gmail.com>
2024-12-05 22:54:17 +0500

    New Day 1 - 2024

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,2 +1,21 @@
-locationIDs = open("./locationIDs.txt", "r").read()
+import math
 
+list1 = []
+list2 = []
+
+with open("./locationIDs.txt", 'r') as ids:
+	for id in ids:
+		lists = id.split()
+
+		list1.append(lists[0])
+		list2.append(lists[1])
+
+list1 = sorted(list1)
+list2 = sorted(list2)
+
+iter = 0
+distance = []
+for l1 in range(0 , len(list1)):
+    distance.append(abs(int(list1[l1]) - int(list2[l1])))
+
+print(sum(distance))
